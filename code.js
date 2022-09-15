591ac9d0fb755d5286f2d96d635bd136814e1591
DHARUN KUMAR <85163456+Dharunkumarsk04@users.noreply.github.com>
2022-09-15 23:47:14 +0530

    Update code.js

--- code.js
+++ code.js
@@ -1,49 +1,60 @@
-const puppeteer = require('puppeteer');         // Require Puppeteer module
+// const puppeteer = require('puppeteer');         // Require Puppeteer module
  
-const url = "https://devops.beemyguest.jp/job/test/lastBuild/logText/progressiveText?start=0";           // Set website you want to screenshot
+// const url = "https://devops.beemyguest.jp/job/test/lastBuild/logText/progressiveText?start=0";           // Set website you want to screenshot
  
-const Screenshot = async () => {                // Define Screenshot function
+// const Screenshot = async () => {                // Define Screenshot function
  
-//    const browser = await puppeteer.launch();    // Launch a "browser"
-   const PCR = require("puppeteer-chromium-resolver");
-   const option = {
-       revision: "",
-       detectionPath: "",
-       folderName: ".chromium-browser-snapshots",
-       defaultHosts: ["https://storage.googleapis.com", "https://npm.taobao.org/mirrors"],
-       hosts: [],
-       cacheRevisions: 2,
-       retry: 3,
-       silent: false
-   };
-   const stats = await PCR(option);
-   const browser = await stats.puppeteer.launch({
-        headless: false,
-        args: ["--no-sandbox"],
-        executablePath: stats.executablePath
-    }).catch(function(error) {
-        console.log(error);
-    });
+// //    const browser = await puppeteer.launch();    // Launch a "browser"
+//    const PCR = require("puppeteer-chromium-resolver");
+//    const option = {
+//        revision: "",
+//        detectionPath: "",
+//        folderName: ".chromium-browser-snapshots",
+//        defaultHosts: ["https://storage.googleapis.com", "https://npm.taobao.org/mirrors"],
+//        hosts: [],
+//        cacheRevisions: 2,
+//        retry: 3,
+//        silent: false
+//    };
+//    const stats = await PCR(option);
+//    const browser = await stats.puppeteer.launch({
+//         headless: false,
+//         args: ["--no-sandbox"],
+//         executablePath: stats.executablePath
+//     }).catch(function(error) {
+//         console.log(error);
+//     });
 
-//    const browser = await puppeteer.launch({
-//      executablePath: '/usr/bin/chromium'
-//    })
-   const page = await browser.newPage();        // Open a new page
+// //    const browser = await puppeteer.launch({
+// //      executablePath: '/usr/bin/chromium'
+// //    })
+//    const page = await browser.newPage();        // Open a new page
  
-   await page.goto(url);                        // Go to the website
+//    await page.goto(url);                        // Go to the website
  
-   await page.screenshot({                      // Screenshot the website using defined options
+//    await page.screenshot({                      // Screenshot the website using defined options
  
-    path: "./screenshot.png",                   // Save the screenshot in current directory
+//     path: "./screenshot.png",                   // Save the screenshot in current directory
  
-    fullPage: true                              // take a fullpage screenshot
+//     fullPage: true                              // take a fullpage screenshot
  
-  });
+//   });
  
-  await page.close();                           // Close the website
+//   await page.close();                           // Close the website
  
-  await browser.close();                        // Close the browser
+//   await browser.close();                        // Close the browser
  
-}
+// }
  
-Screenshot();                                   // Call the Screenshot function
+// Screenshot();                                   // Call the Screenshot function
+
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://google.com');
+  await page.screenshot({path: 'example.png'});
+
+  await browser.close();
+})();
